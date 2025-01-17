5d9303774dfe8f3212f3ad883e81d1c0d9cd709e
Mathytics <mathlytic4002@gmail.com>
2025-01-17 16:56:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,68 @@
 const topics = {
-    form2: [
-        { name: "Three Dimensional Shapes", urlNotes: "https://example.com/form2/3d-notes", urlExercises: "https://example.com/form2/3d-exercises", urlQuizes: "https://example.com/form2/quizzes", urlPastYear: "https://example.com/form2/past-year" },
-        { name: "Cartesian Plane", urlNotes: "https://example.com/form2/cartesian-notes", urlExercises: "https://example.com/form2/cartesian-exercises", urlQuizes: "https://example.com/form2/quizzes", urlPastYear: "https://example.com/form2/past-year" }
-    ],
+
     form4: [
         { 
-            name: "Operation on Sets", 
+            name: "Chapter 3: Logical Reasoning", 
             urlNotes: "https://drive.google.com/file/d/1NqdfACzqxtk0JufWLmfHZ50DWAYS6bTN/view", 
             urlExercises: "https://example.com/form4/sets-exercises", 
             urlQuizes: "https://example.com/form4/quizzes", 
             urlPastYear: "https://example.com/form4/past-year",
             subtopics: [
                 { 
-                    name: "4.1: Three Dimensional Shapes", 
-                    urlNotes: "https://example.com/form2/3d-notes", 
-                    urlExercises: "https://example.com/form2/3d-exercises", 
-                    urlQuizes: "https://example.com/form2/quizzes", 
-                    urlPastYear: "https://example.com/form2/past-year" 
+                    name: "3.1: Statements", 
+                    urlNotes: "https://drive.google.com/file/d/1XN9v0JmYM5CTCup2ZH_MknkLENjeWrZh/view?usp=drive_link", 
+                    urlExercises: "https://drive.google.com/file/d/1fH6DfVaYVW81luXEhGXeLAewIfz5ZVXY/view?usp=drive_link", 
+                    urlQuizes: "https://quizizz.com/join?gc=26597520", 
+                    urlPastYear: "https://drive.google.com/drive/folders/1U7QRjrQ3h-NyCJkrzAsbDJvg15AQWgem?usp=drive_link",
+                    urlRPH: "https://docs.google.com/document/d/13GOjOCprZSO9REfblE2EweZbQp1CDb-B/edit?usp=drive_link&ouid=100255213395062774814&rtpof=true&sd=true",
+                    urlInteractive:"https://wordwall.net/resource/37067040/ting-4-bab-3-tp2"
                 },
                 { 
-                    name: "4.2: Cartesian Plane", 
-                    urlNotes: "https://example.com/form2/cartesian-notes", 
-                    urlExercises: "https://example.com/form2/cartesian-exercises", 
-                    urlQuizes: "https://example.com/form2/quizzes", 
-                    urlPastYear: "https://example.com/form2/past-year" 
+                    name: "3.2: Arguments", 
+                    urlNotes: "https://drive.google.com/file/d/1TZwf4Cg0AYcJ6ZuT6GBE9QtKrkaOInb1/view?usp=drive_link", 
+                    urlExercises: "https://drive.google.com/file/d/1igMXsahIq7amwOmNo8APHtu9lpZaYXzi/view?usp=drive_link", 
+                    urlQuizes: "https://quizizz.com/join?gc=05945488", 
+                    urlPastYear: "https://drive.google.com/drive/folders/1U7QRjrQ3h-NyCJkrzAsbDJvg15AQWgem?usp=drive_link",
+                    urlRPH: "https://docs.google.com/document/d/13GOjOCprZSO9REfblE2EweZbQp1CDb-B/edit?usp=drive_link&ouid=100255213395062774814&rtpof=true&sd=true",
+                    urlInteractive:"https://wordwall.net/resource/37067040/ting-4-bab-3-tp2"
                 }
             ]
         },
         { 
-            name: "Logical Reasoning", 
-            urlNotes: "https://drive.google.com/drive/folders/1G3YTL-3abMbn", 
-            urlExercises: "https://drive.google.com/file/d/1xO333B7WJRyVZD5o9HhI46HB4NcWp0kd/view", 
-            urlQuizes: "https://quizizz.com/join?gc=33531104", 
-            urlPastYear: "https://drive.google.com/drive/folders/1yp3FVYg_RXKXHmq6T3eWRGmAAEI-ie8o" 
+            name: "Chapter 4: Operation on Sets", 
+            urlNotes: "https://drive.google.com/file/d/1NqdfACzqxtk0JufWLmfHZ50DWAYS6bTN/view", 
+            urlExercises: "https://example.com/form4/sets-exercises", 
+            urlQuizes: "https://example.com/form4/quizzes", 
+            urlPastYear: "https://example.com/form4/past-year",
+            subtopics: [
+                { 
+                    name: "4.1: Intersection on Sets", 
+                    urlNotes: "https://drive.google.com/file/d/1jR9jcmvvDsH7aYJuwt9VN1OqV_bZB5PW/view?usp=drive_link", 
+                    urlExercises: "https://drive.google.com/file/d/16LdEEX3EjjNPbjR9JqlOqgEvCer0xXj1/view?usp=drive_link", 
+                    urlQuizes: "https://quizizz.com/join/quiz/67606abff3407fd2a85125ff/start?studentShare=true", 
+                    urlPastYear: "https://drive.google.com/drive/folders/1R5WCZN02Nqs_k9JlJGItU9njGyu0igBa?usp=drive_link",
+                    urlRPH: "https://docs.google.com/document/d/1DCDMuq2uWgaeTGhJlePzk7LVULkBO92d/edit?usp=drive_link&ouid=100255213395062774814&rtpof=true&sd=true",
+                    urlInteractive:"https://wordwall.net/resource/37070451"
+                },
+                { 
+                    name: "4.2: Union of Sets", 
+                    urlNotes: "https://drive.google.com/file/d/1xgzFA_qApr47FwkcBaAamjGckI5m7MRA/view?usp=drive_link", 
+                    urlExercises: "https://drive.google.com/file/d/1QOfFQ7xaGZKzaAjT3UCj9JThB-6FjMmS/view?usp=drive_link", 
+                    urlQuizes: "https://quizizz.com/join/quiz/67608d17adc567e4a6ad9d51/start?studentShare=true", 
+                    urlPastYear: "https://drive.google.com/drive/folders/1R5WCZN02Nqs_k9JlJGItU9njGyu0igBa?usp=drive_link",
+                    urlRPH: "https://docs.google.com/document/d/1DCDMuq2uWgaeTGhJlePzk7LVULkBO92d/edit?usp=drive_link&ouid=100255213395062774814&rtpof=true&sd=true",
+                    urlInteractive:"https://wordwall.net/resource/37070451"
+                },
+                 { 
+                    name: "4.3: Combined Operations on Sets", 
+                    urlNotes: "https://drive.google.com/file/d/10xzPhm3wMOO49_geFyHgG4mn1xzmu4mJ/view?usp=drive_link", 
+                    urlExercises: "https://drive.google.com/file/d/1nLf8NDyRbVXf7CFjoUua0IvgAb4Hs5Ce/view?usp=drive_link", 
+                    urlQuizes: "https://quizizz.com/join/quiz/677b585e4e05e009e275d01b/start?studentShare=true", 
+                    urlPastYear: "https://drive.google.com/drive/folders/1R5WCZN02Nqs_k9JlJGItU9njGyu0igBa?usp=drive_link",
+                    urlRPH: "https://docs.google.com/document/d/1DCDMuq2uWgaeTGhJlePzk7LVULkBO92d/edit?usp=drive_link&ouid=100255213395062774814&rtpof=true&sd=true",
+                    urlInteractive:"https://wordwall.net/resource/37070451"
+                }
+            ]
         }
     ]
 };
@@ -92,6 +122,8 @@ function showLinks() {
                 <a href="${selectedTopic.urlExercises}" target="_blank">View Exercises</a>
                 <a href="${selectedTopic.urlQuizes}" target="_blank">View Quizzes</a>
                 <a href="${selectedTopic.urlPastYear}" target="_blank">View Past Year</a>
+                <a href="${selectedTopic.urlRPH}" target="_blank">E-RPH</a>
+                <a href="${selectedTopic.urlInteractive}" target="_blank">Interactive Lesson</a>
             `;
         }
     } else {
@@ -127,3 +159,4 @@ function submitQuery() {
 }
 
 window.onload = loadMessages;
+
